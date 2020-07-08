5c32e9cf9d87a76f66715626c0bc74aa0cd83233
Mike Hepburn <eformat@gmail.com>
2020-07-08 15:23:43 +1000

    🦀 add more warmups 🦀

--- warmup-cache.sh
+++ warmup-cache.sh
@@ -6,7 +6,8 @@ NEXT=1800
 PAST=0
 
 callLatLong() {
-    http --timeout 240 ${SCROUTE}/api/nearby-departures/${LATLONG}/${DIST}?nextSeconds=${NEXT}\&pastSeconds=${PAST}
+    #http --timeout 240 ${SCROUTE}/api/nearby-departures/${LATLONG}/${DIST}?nextSeconds=${NEXT}\&pastSeconds=${PAST}
+    hey -t 120 -c 10 -n 10 ${SCROUTE}/api/nearby-departures/${LATLONG}/${DIST}?nextSeconds=${NEXT}\&pastSeconds=${PAST}
 }
 
 LATLONG=-37.7968151,144.9507116
@@ -21,17 +22,30 @@ LATLONG=-37.8183886,144.9524854
 callLatLong
 LATLONG=-37.8974484,145.088703
 callLatLong
-
+LATLONG=-37.8143622,144.9675997
+callLatLong
 
 callSearch() {
-    http --timeout 240 ${SCROUTE}/api/search-departures/${SEARCH}
+    #http --timeout 240 ${SCROUTE}/api/search-departures/${SEARCH}
+    hey -t 120 -c 10 -n 10 ${SCROUTE}/api/search-departures/${SEARCH}
 }
 
-SEARCH=central
+SEARCH=central?routeType=2
 callSearch
-SEARCH=north
+SEARCH=north?routeType=3
 callSearch
-SEARCH=melbourne%20zoo
+SEARCH=melbourne%20zoo?routeType=2
 callSearch
-SEARCH=toorak
+SEARCH=toorak?routeType=2
 callSearch
+SEARCH=Lara?routeType=1
+callSearch
+SEARCH=Lara?routeType=2
+callSearch
+SEARCH=Lara?routeType=3
+callSearch
+SEARCH=haines?routeType=1
+callSearch
+SEARCH=haines?routeType=2
+callSearch
+
